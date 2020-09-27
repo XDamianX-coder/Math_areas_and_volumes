981db96113aaf43e4d585bc866bfd3d03a81fd6a
XDamianX-coder <52101130+XDamianX-coder@users.noreply.github.com>
2020-09-27 08:29:01 +0200

    Update trapeze_square.py

--- trapeze_square.py
+++ trapeze_square.py
@@ -52,6 +52,25 @@ h1 = float(h)
 baseData = 0.5*(a1+b1)*h1
 baseData
 
+st.header('Parallelogram area'.)
+
+dc = float(23.56)
+dc1 = float(123.654)
+dd = st.sidebar.text_area("The base lenght of the parallelogram.", dc)
+dd1 = st.sidebar.text_area("The height of the parallelogram.", dc1)
+ddd = float(dd)
+ddd1 = float(dd1)
+baseData5 = ddd*ddd1
+
+st.header('Cirlce area'.)
+pi1 = float(22)
+pi2 = float(7)
+pi3 = float(pi1/pi2)
+rr = float(23.65412)
+rr1 = st.sidebar.text_area("The radius of the circle.", rr)
+rr11 = float(rr1)
+basecircle = pi3*rr11*rr11
+
 
 abc = float(87.7654)
 st.header('Volume of the cube.')
@@ -60,3 +79,7 @@ abc1 = st.sidebar.text_area("The edge of the cube.", abc)
 abc1 = float(abc)
 base2 = abc*abc*abc
 base2
+
+
+
+
